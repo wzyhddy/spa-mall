e4c74e7ba8ce7c6be56964918f5b8eeab76b0ecc
诉衷情の麻雀 <2783713236@qq.com>
2025-05-03 13:39:35 +0800

    feat:增加限流功能

---
+++ mall-business/src/main/java/com/net/sparrow/annotation/Limit.java
@@ -0,0 +1,45 @@
+package com.net.sparrow.annotation;
+
+import com.net.sparrow.enums.LimitTypeEnum;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 限流注解
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Limit {
+
+    /**
+     * 资源的key,唯一
+     * 作用：不同的接口，不同的流量控制
+     */
+    String key() default "";
+
+    /**
+     * 最多的访问限制次数
+     */
+    int permitsPerSecond();
+
+    /**
+     * 获取令牌最大等待时间
+     */
+    long timeOut();
+
+    /**
+     * 获取令牌最大等待时间,单位(例:分钟/秒/毫秒) 默认:毫秒
+     */
+    TimeUnit timeUnit() default TimeUnit.MILLISECONDS;
+
+    /**
+     * 限流类型，默认是整个接口
+     */
+    LimitTypeEnum limitType() default LimitTypeEnum.ALL;
+}
---
+++ mall-business/src/main/java/com/net/sparrow/enable/EnableSimpleLimit.java
@@ -0,0 +1,17 @@
+package com.net.sparrow.enable;
+
+import com.net.sparrow.config.SimpleLimitConfig;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import({SimpleLimitConfig.class})
+public @interface EnableSimpleLimit {
+}
